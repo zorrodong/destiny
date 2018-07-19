8d1c1b1e0ff049c7a98b1db266c68847766216d7
Philipp A <flying-sheep@web.de>
2018-07-19 10:39:04 +0200

    Switched score

--- inst/playground-new.r
+++ inst/playground-new.r
@@ -1,14 +1,16 @@
 library(Matrix)
 library(ggplot2)
+library(patchwork)
 
-if (!'env_playground' %in% search()) {
-	attach(new.env(), name = 'env_playground')
+import_env <- 'env_playground'
+if (!import_env %in% search()) {
+	attach(new.env(), name = import_env)
 	assign('import_from', function(pkg, names) {
 		for (name in names) {
 			val <- get(name, asNamespace(pkg), inherits = FALSE)
-			assign(name, val, as.environment('env_playground'))
+			assign(name, val, as.environment(import_env))
 		}
-	}, as.environment('env_playground'))
+	}, as.environment(import_env))
 }
 
 
@@ -21,20 +23,20 @@ import_from('destiny', c('dataset_extract_doublematrix', 'get_smoothing', 'get_l
 plot_gr <- function(dm, gr, n_gene_clusts = 500L, dims = 1:2, k = 30, distance = c('euclidean', 'cosine', 'rankcor')) {
 	#d <- dataset_extract_doublematrix(dataset(dm))
 	distance <- match.arg(distance)
-	
+
 	cell_clust_label <- get_louvain_clusters(dm@transitions)
 	cell_clusts <- seq_len(max(cell_clust_label))
-	
+
 	best_genes <- lapply(cell_clusts, function(cc) {
 		chunk <- gr@exprs[cell_clust_label == cc, ]
 		dists <- find_knn(t(chunk), k, distance = distance)$dist_mat
 		adj <- sparseMatrix(dists@i, p = dists@p, x = 1/dists@x, dims = dim(dists), symmetric = TRUE, index1 = FALSE)
-		
+
 		# no resolution settable, so using one of the hierarchical: cluster_walktrap, cluster_fast_greedy, cluster_edge_betweenness
 		graph <- igraph::graph_from_adjacency_matrix(adj, 'undirected', weighted = TRUE)
 		gene_clust_label <- as.integer(unclass(igraph::cut_at(igraph::cluster_walktrap(graph), n_gene_clusts)))
 		gene_clusts <- seq_len(max(gene_clust_label))
-		
+
 		gene_clust_scores <- lapply(gene_clusts, function(cg) {
 			gene_names <- Biobase::featureNames(gr)[gene_clust_label == cg]
 			scores <- gr@partials_norm[cell_clust_label == cc, gene_clust_label == cg, drop = FALSE]
@@ -44,7 +46,7 @@ plot_gr <- function(dm, gr, n_gene_clusts = 500L, dims = 1:2, k = 30, distance =
 		gene_clust_score_means <- sapply(gene_clusts, function(cg) mean(gene_clust_scores[[cg]]))
 		names(gene_clust_scores[[which.max(gene_clust_score_means)]])
 	})
-	
+
 	plot_data <- data.frame(gr@coords, Cluster = sapply(best_genes[cell_clust_label], paste, collapse = ', '))
 	gg <- ggplot(plot_data, aes(DC1, DC2, colour = Cluster)) + geom_point()
 	gg$ids <- best_genes
@@ -65,35 +67,75 @@ scale_fill_cluster <- function(...) scale_fill_manual(values = prettier_colors, 
 
 ggplot(tibble::enframe(prettier_colors), aes(name, 1, fill = value)) +
 	geom_col() + coord_fixed() + scale_fill_identity() + labs(x = NULL, y = NULL) +
-	theme_void() + theme(axis.text.x = element_text())
+	theme_void() + theme(axis.text.x = element_text(size = 8, angle = 0, debug = FALSE))
 
 
 # Test code ----
 
 
-import_from('destiny', c('DiffusionMap', 'gene_relevance'))
+import_from('destiny', c('DiffusionMap', 'gene_relevance', 'plot_gene_relevance'))
 import_from('plotly', c('ggplotly', 'export'))
 import_from('glue', 'glue')
+import_from('SummarizedExperiment', 'rowData')
 
 scial <- readRDS('~/Analysis/destiny-paper/scialdone.rds')
-scial_blood <- subset(scial, rep(TRUE, nrow(scial)), scial$cluster %in% c('darkorange', 'brown'))
+# scial_blood <- subset(scial, rep(TRUE, nrow(scial)), scial$cluster %in% c('yellow', 'brown'))
 
-do_it <- function(dat, name, distnc) {
-	dm_scial <- DiffusionMap(dat, distance = distnc)
-	gr_scial <- gene_relevance(dm_scial, smooth = FALSE)
-	featureNames(gr_scial) <- SummarizedExperiment::rowData(dat)$Symbol
-	gm_scial <- plot_gr(dm_scial, gr_scial)
-	gm_scial_ly <- ggplotly(gm_scial + scale_colour_hue())
-	print(gm_scial_ly)
-	
-	dir.create('mail', showWarnings = FALSE)
-	base <- glue('mail/{name} {distnc}')
-	export(ggplotly(plot(dm_scial, 1:2, col_by = 'cluster') + scale_fill_cluster()), glue('{base}.png'))
-	export(gm_scial_ly, glue('{base} gene relevance v2.png'))
-	writeChar(jsonlite::toJSON(setNames(gm_scial$ids, 1:4), pretty = TRUE), glue('{base} gene relevance v2.json'))
-}
+# do_it <- function(dat, name, distnc) {
+# 	dm <- DiffusionMap(dat, distance = distnc)
+# 	gr <- gene_relevance(dm, smooth = FALSE)
+# 	featureNames(gr) <- SummarizedExperiment::rowData(dat)$Symbol
+# 	gm <- plot_gr(dm, gr)
+# 	dm_ly <- ggplotly(plot(dm, 1:2, col_by = 'cluster') + scale_fill_cluster())
+# 	gm_ly <- ggplotly(gm + scale_colour_hue())
+# 	print(dm_ly)
+# 	print(gm_ly)
+# 	
+# 	dir.create('mail', showWarnings = FALSE)
+# 	base <- glue('mail/{name} {distnc}')
+# 	export(dm_ly, glue('{base}.png'))
+# 	export(gm_ly, glue('{base} gene relevance v2.png'))
+# 	writeChar(jsonlite::toJSON(setNames(gm$ids, seq_len(min(length(gm$ids), 4))), pretty = TRUE), glue('{base} gene relevance v2.json'))
+# 	
+# 	gm
+# }
+# 
+# if (!('maps' %in% ls())) maps <- list()
+# for (distnc in c('rankcor', 'cosine', 'euclidean')) {
+# if (!distnc %in% names(maps)) maps[[distnc]] <- list(
+# 	normal = do_it(scial,     'normal', distnc),
+# 	blood = do_it(scial_blood, 'blood', distnc)
+# )
+# }
 
-for (distnc in c('euclidean', 'cosine', 'rankcor')) {
-	do_it(scial,       'normal', distnc)
-	do_it(scial_blood, 'blood',  distnc)
-}
+if (!'dm_scial' %in% ls()) dm_scial <- DiffusionMap(scial, distance = 'rankcor')
+if (!'gr_scial' %in% ls()) gr_scial <- gene_relevance(dm_scial)
+if (!'gr_scial_unsmooth' %in% ls()) gr_scial_unsmooth <- gene_relevance(dm_scial, smooth = FALSE)
+featureNames(gr_scial) <- rowData(scial)$Symbol
+gg_scial <- plot(dm_scial, 1:2, col_by = 'cluster') + scale_fill_cluster()
+gm_scial <- plot_gene_relevance(gr_scial) + ggtitle('GR smoothed partials+F1')
+gm_scial_unsmooth <- plot_gene_relevance(gr_scial_unsmooth, genes = 8) + ggtitle('GR smoothed F1')
+gm_scial_louvain <- plot_gr(dm_scial, gr_scial_unsmooth) + ggtitle('GR louvain clustering')
+
+print(ggplotly(gg_scial))
+print(ggplotly(gm_scial))
+print(ggplotly(gm_scial_unsmooth))
+print(ggplotly(gm_scial_louvain))
+ggsave(
+	'mail/normal rankcor gene relevance v1.png',
+	(plot(gr_scial, gm_scial$ids) | (gm_scial / gg_scial)) + plot_layout(widths = c(3, 1)),
+	width = 12)
+ggsave(
+	'mail/normal rankcor gene relevance v1 unsmoothed.png',
+	(plot(gr_scial, gm_scial_unsmooth$ids) | (gm_scial_unsmooth / gg_scial)) + plot_layout(widths = c(3, 1)),
+	width = 12)
+ggsave(
+	'mail/normal rankcor gene relevance v2 louvain.png',
+	(plot(gr_scial, unlist(gm_scial_louvain$ids)) | (gm_scial_louvain / gg_scial)) + plot_layout(widths = c(3, 1)),
+	width = 12)
+
+
+
+gr_turquoise <- subset(scial, rep(TRUE, nrow(scial)), scial$cluster %in% 'turquoise') %>% DiffusionMap(distance = 'rankcor') %>% gene_relevance()
+featureNames(gr_turquoise) <- rowData(scial)$Symbol
+(plot(gr_turquoise, c('T', 'Mesp1', 'Evx1', 'Id3', 'Wfdc2')) / plot_gene_relevance(gr_turquoise))
